98bac23344b4f4ac11185d36e18016b1cac6665c
lissLiddell <lissetchopper@gmail.com>
2023-02-17 19:00:02 -0600

    buscando usuario parte 2 - 1

--- ajax/searchAjax.php
+++ ajax/searchAjax.php
@@ -4,7 +4,10 @@
 
     if(isset($_POST['initial_search']) || isset($_POST['delete_search']) || isset($_POST['start_date']) || isset($_POST['end_date'])){
         $data_url=[
-            "usuario"=>"user-search"
+            "usuario"=>"user-search",
+            "cliente"=>"client-search",
+            "item"=>"item-search",
+            "prestamo"=>"reservation-search"
         ];
 
         if(isset($_POST['model'])){
@@ -28,6 +31,18 @@
             ];
             echo json_encode($alert);
             exit();
+        }
+
+        if($model=="prestamo"){
+            $start_date="start_date_".$model;
+            $end_date="end_date_".$model;
+
+            // start search 
+            if(isset($_POST['start_date'])){
+
+            }
+        }else{
+
         }
     }else{
         session_unset(); 
